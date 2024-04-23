d7177156f87ec3b5cd203529cbf54049c5fa5d6e
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-04-23 16:33:30 +0530

    Update bstToSortedLinkedList.cpp

--- Data structures in c++/bst/bstToSortedLinkedList.cpp
+++ Data structures in c++/bst/bstToSortedLinkedList.cpp
@@ -96,8 +96,122 @@ using namespace std;
 
 ***********************************************************/
 
+pair<Node<int>*, Node<int>*> helper(BinaryTreeNode<int>* root){
+	//  if root is null -- return null
+	if(root == NULL){
+		pair<Node<int>*, Node<int>*> ans;
+		ans.first = NULL;
+		ans.second = NULL;
+		return ans;
+	}
+
+	// HERE .first = HEAD and .second = TAIL
+
+
+	// NOW THERE ARE 4 CASES ---
+	// 1) if both left and right are not null then ======= left/root->data/right
+	// 2) if both are null then ======== root->data as head and tail both
+	// 3) if left is null and right is not null then ====== root->data/right
+	// 4) if right is null and left is not null then ====== left/root->data
+
+	// if both left and right is not null -- then starting is leftHead and ending is rightTail
+	if(root -> left != NULL && root -> right != NULL){
+		pair<Node<int>*, Node<int>*> left = helper(root -> left);
+		pair<Node<int>*, Node<int>*> right = helper(root -> right);
+
+		Node<int>* head = left.first;
+
+		// middle will be root data
+		Node<int>* middle = new Node<int>(root -> data);
+		
+		// next of tail of left will be root data 
+		left.second -> next = middle;
+		// and root data's next is right's head
+		middle -> next = right.first;
+
+		pair<Node<int>*, Node<int>*> ans;
+		ans.first = head;					//head	 
+		ans.second = right.second;		// tail
+		return ans;
+	}
+
+	// left is null and right is null --- then both head and tail  = root data
+	else if(root -> left == NULL && root -> right == NULL){
+		pair<Node<int>*, Node<int>*> ans;
+		Node<int>* middle = new Node<int>(root -> data);
+		ans.first = middle;
+		ans.second = middle;
+		return ans;
+	}
+
+	// if left is null and right is not null
+	else if(root -> left == NULL && root -> right != NULL){
+		pair<Node<int>*, Node<int>*> right = helper(root -> right);
+
+		pair<Node<int>*, Node<int>*> ans;
+		Node<int>* middle = new Node<int>(root -> data);
+
+		middle -> next = right.first;
+		
+		ans.first = middle;						// head will be root's data
+		ans.second = right.second;		// tail right.second
+		return ans;
+	}
+
+	// if left is not null and right is null
+	else if(root -> left != NULL && root -> right == NULL){
+		pair<Node<int>*, Node<int>*> left = helper(root -> left);
+
+		pair<Node<int>*, Node<int>*> ans;
+		Node<int>* middle = new Node<int>(root -> data);
+
+		Node<int>* head = left.first;
+		left.second -> next = middle;
+		// middle -> next == NULL;
+
+		ans.first = head;				// head would be left.first
+		ans.second = middle;		//tail would be middle
+		return ans;
+	}	
+}
+
+
+pair<Node<int>*, Node<int>*> helper2(BinaryTreeNode<int>* root){
+	if(root == NULL){
+		pair<Node<int>*, Node<int>*>ans;
+		ans.first = NULL;
+		ans.second = NULL;
+		return ans;
+	}
+	
+	pair<Node<int>*, Node<int>*>left = helper2(root->left);
+	pair<Node<int>*, Node<int>*>right = helper2(root->right);
+
+	Node<int>* temp = new Node<int>(root->data);
+
+	if(left.first == NULL && left.second == NULL){
+		pair<Node<int>*, Node<int>*>ans;
+		ans.first = temp;
+		ans.second = temp;
+		return ans;
+	}
+	else{
+		left.second->next = temp;
+		temp->next = right.first;
+	}
+
+	pair<Node<int>*, Node<int>*>res;
+	res.first = left.first;
+	res.second = right.second;
+
+	return res;
+}
+
+
 Node<int>* constructLinkedList(BinaryTreeNode<int>* root) {
 	// Write your code here
+	pair<Node<int>*, Node<int>*> ans = helper(root);
+
 }
 
 BinaryTreeNode<int>* takeInput() {
